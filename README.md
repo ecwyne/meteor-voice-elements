341a1f5a618f27d86335d81e0d5814022649c1d7
Eric Wyne <ecwyne@gmail.com>
2014-09-17 13:02:18 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+#meteor-voice-elements
+
+##About
+Bring the power of [voice-elements](https://github.com/zenorocha/voice-elements) to Meteor!
+
+##Install
+```bach
+meteor add ecwyne:voice-elements
+```
+
+##Docs
+Simply follow these [instructions](https://github.com/zenorocha/voice-elements#usage). Step 1 is completed for you!
---
+++ package.js
@@ -0,0 +1,13 @@
+Package.describe({
+	summary: "Web Speach API for Meteor!",
+	version: "1.0.0",
+	git: "http://github.com/ecwyne/meteor-voice-elements",
+	name: 'ecwyne:voice-elements'
+});
+
+Package.onUse(function(api) {
+	api.use('underscore');
+	api.use('ecwyne:polymer@1.0.1')
+	api.versionsFrom('METEOR@0.9.0');
+	api.addFiles('voice-elements.js', server);
+});
